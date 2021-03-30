78448a5ec829795ae4f8ff78f384e2b2b75a3fe3
West <k.west2@snhu.edu>
2021-03-30 15:11:13 -0400

    forgot to save a few files before last commit

--- R/exec_test_summary.R
+++ R/exec_test_summary.R
@@ -8,10 +8,10 @@ source(here::here("R","create_test_summary.R"))
 
 # creates test summary table
 create_test_summary(
-  tbl_name = "auto_vm_verification_", # name of image
+  tbl_name = "smf_no_app_gr_", # name of image
 
-  input_vars = c("Auto Voicemail SFS Verification Dialer",
-                 "1/14/21 - 3/1/21", # Date range of test
+  input_vars = c("SMF No App Navigator (GR)",
+                 "2/26/21 - 3/26/21", # Date range of test
                  "File Complete Rate - Increased Slightly", # Test result 
                  "TBD" ) # Recommendation
 )
